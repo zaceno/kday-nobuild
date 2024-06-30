2150f2b14609242385abb8d79546ec108a4efc82
zaceno <zacharias.enochsson@gmail.com>
2024-06-30 23:04:36 +0200

    Update for presentation

--- 4-importmaps/App.js
+++ 4-importmaps/App.js
@@ -2,7 +2,8 @@ import html from "html"
 import Counter from "@/Counter.js"
 
 export default () => {
-  return html` <main>
+  return html`
+  <main>
     <h2>Counter Demo - 4. Importmaps </h2>
     <${Counter} />
   </main>`
--- 5-microframeworks/App.js
+++ 5-microframeworks/App.js
@@ -1,14 +1,15 @@
 import html from "html"
 import Counter from "@/Counter.js"
 
-export default ({ state, setState }) => html` <main>
-  <h2>Counter Demo - 5. Microframeworks</h2>
-  <${Counter}
-    value=${state.counter}
-    setValue=${counter =>
-      setState({
-        ...state,
-        counter,
-      })}
-  />
-</main>`
+export default ({ state, setState }) => html`
+  <main>
+    <h2>Counter Demo - 5. Microframeworks</h2>
+    <${Counter}
+      value=${state.counter}
+      setValue=${counter =>
+        setState({
+          ...state,
+          counter,
+        })}
+    />
+  </main>`
