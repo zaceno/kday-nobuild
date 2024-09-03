acbd549cc50f92f11cf9dc2634a4f6e082cd2d8a
Zacharias Enochsson <zacharias.enochsson@gmail.com>
2024-09-03 22:42:31 +0200

    update for presentation

--- 7-types/Counter.js
+++ 7-types/Counter.js
@@ -1,10 +1,9 @@
 import { div, h1, button, text } from "html"
 
 /**
- * @template S -- State type of app using component
  * @param {object} props
  * @param {number} props.value -- value of counter
- * @param {import('hyperapp').Action<S, number>} props.setValue -- action to set value
+ * @param {import('hyperapp').Action<any, number>} props.setValue -- action to set value
  */
 export default ({ value, setValue }) =>
   div([
